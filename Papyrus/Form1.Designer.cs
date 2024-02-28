56dc3c468301d8a70701c710d570b3127727f92e
Zak Cole <zakcole08@gmail.com>
2024-02-28 18:18:38 +0000

    Add project files.

---
+++ Papyrus/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Papyrus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.saveBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.fontSelect = new System.Windows.Forms.ToolStripComboBox();
+            this.fontSizeSelect = new System.Windows.Forms.ToolStripComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtMain = new System.Windows.Forms.RichTextBox();
+            this.toolPanel = new System.Windows.Forms.Panel();
+            this.txtLineNums = new System.Windows.Forms.RichTextBox();
+            this.toolStrip.SuspendLayout();
+            this.toolPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(236)))), ((int)(((byte)(188)))));
+            this.toolStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton2,
+            this.toolStripButton1,
+            this.saveBar,
+            this.toolStripSeparator1,
+            this.fontSelect,
+            this.fontSizeSelect});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(978, 38);
+            this.toolStrip.TabIndex = 0;
+            this.toolStrip.Text = "toolStrip1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(242)))), ((int)(((byte)(255)))));
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(34, 33);
+            this.toolStripButton2.Text = "btnOpen";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(242)))), ((int)(((byte)(255)))));
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(34, 33);
+            this.toolStripButton1.Text = "btnSave";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // saveBar
+            // 
+            this.saveBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.saveBar.Name = "saveBar";
+            this.saveBar.Size = new System.Drawing.Size(100, 33);
+            this.saveBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 38);
+            // 
+            // fontSelect
+            // 
+            this.fontSelect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.fontSelect.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fontSelect.Items.AddRange(new object[] {
+            "AGA Arabesque Desktop",
+            "Agency FB",
+            "Akhbar MT",
+            "ALGERIAN",
+            "Arial",
+            "Arial Rounded MT",
+            "Art",
+            "Bahnschrift",
+            "Baskerville Old Face",
+            "Bell MT",
+            "Berlin Sans FB",
+            "Bernard MT",
+            "Blackadder ITC",
+            "Bodoni MT",
+            "Bodoni MT Poster",
+            "Book Antiqua",
+            "Bookman Old Style",
+            "Bookshelf Symbol 7",
+            "Bouhout 95",
+            "Bradley Hand ITC",
+            "Britannic",
+            "Broadway",
+            "Brush Script MT",
+            "Calibri",
+            "Californian FB",
+            "Calisto MT",
+            "Cambria",
+            "Cambria Math",
+            "Candara",
+            "Cascadia Code",
+            "Cascadia Mono",
+            "Castellar",
+            "Centaur",
+            "Century",
+            "Century Gothic",
+            "Century Schoolbook",
+            "Chiller",
+            "Colonna MT",
+            "Comic Sans MS",
+            "Consolas",
+            "Constantia"});
+            this.fontSelect.Name = "fontSelect";
+            this.fontSelect.Size = new System.Drawing.Size(121, 38);
+            this.fontSelect.Sorted = true;
+            this.fontSelect.Text = "Consolas";
+            this.fontSelect.SelectedIndexChanged += new System.EventHandler(this.fontSelect_SelectedIndexChanged);
+            this.fontSelect.Click += new System.EventHandler(this.toolStripComboBox1_Click);
+            // 
+            // fontSizeSelect
+            // 
+            this.fontSizeSelect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.fontSizeSelect.Items.AddRange(new object[] {
+            "2",
+            "4",
+            "6",
+            "8",
+            "10",
+            "12",
+            "14",
+            "16",
+            "18",
+            "20"});
+            this.fontSizeSelect.Name = "fontSizeSelect";
+            this.fontSizeSelect.Size = new System.Drawing.Size(75, 38);
+            this.fontSizeSelect.Text = "10";
+            this.fontSizeSelect.SelectedIndexChanged += new System.EventHandler(this.fontSizeSelect_IndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.Color.Cornsilk;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(0, 644);
+            this.panel1.TabIndex = 1;
+            // 
+            // txtMain
+            // 
+            this.txtMain.AcceptsTab = true;
+            this.txtMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMain.AutoWordSelection = true;
+            this.txtMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(253)))), ((int)(((byte)(243)))));
+            this.txtMain.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMain.BulletIndent = 1;
+            this.txtMain.EnableAutoDragDrop = true;
+            this.txtMain.Location = new System.Drawing.Point(142, 38);
+            this.txtMain.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtMain.Name = "txtMain";
+            this.txtMain.Size = new System.Drawing.Size(836, 606);
+            this.txtMain.TabIndex = 2;
+            this.txtMain.Text = "";
+            this.txtMain.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // toolPanel
+            // 
+            this.toolPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.toolPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(205)))));
+            this.toolPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.toolPanel.Controls.Add(this.txtLineNums);
+            this.toolPanel.Location = new System.Drawing.Point(0, 38);
+            this.toolPanel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.toolPanel.Name = "toolPanel";
+            this.toolPanel.Size = new System.Drawing.Size(134, 606);
+            this.toolPanel.TabIndex = 3;
+            this.toolPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // txtLineNums
+            // 
+            this.txtLineNums.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtLineNums.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(233)))), ((int)(((byte)(195)))));
+            this.txtLineNums.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtLineNums.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtLineNums.Location = new System.Drawing.Point(84, 0);
+            this.txtLineNums.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtLineNums.Name = "txtLineNums";
+            this.txtLineNums.ReadOnly = true;
+            this.txtLineNums.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtLineNums.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.txtLineNums.Size = new System.Drawing.Size(50, 623);
+            this.txtLineNums.TabIndex = 0;
+            this.txtLineNums.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(253)))), ((int)(((byte)(243)))));
+            this.ClientSize = new System.Drawing.Size(978, 644);
+            this.Controls.Add(this.toolStrip);
+            this.Controls.Add(this.toolPanel);
+            this.Controls.Add(this.txtMain);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Papyrus";
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            this.toolPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox txtMain;
+        private System.Windows.Forms.Panel toolPanel;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.RichTextBox txtLineNums;
+        private System.Windows.Forms.ToolStripComboBox fontSelect;
+        private System.Windows.Forms.ToolStripProgressBar saveBar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripComboBox fontSizeSelect;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+    }
+}
+
---
+++ Papyrus/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Papyrus
+{
+    public partial class Form1 : Form
+    {
+        private int lastLineCount = 1;
+        int lineNum = 1;
+        SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtLineNums.Text = $" | {lineNum}\n";
+            LoadFileFromCommandLine();
+        }
+
+        private void LoadFileFromCommandLine()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+
+            if (args.Length > 1)
+            {
+                try
+                {
+                    string filePath = args[1]; // The first command line argument is the file path
+
+                    using (System.IO.StreamReader sr = new System.IO.StreamReader(filePath))
+                    {
+                        string fileContents = sr.ReadToEnd();
+                        txtMain.Text = fileContents;
+                    }
+
+                    printLineNums();
+                    saveBar.Value = 100;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error opening file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        void printLineNums()
+        {
+            string[] lines = txtMain.Lines;
+
+            while (lines.Length > lastLineCount)
+            {
+                lineNum++;
+                txtLineNums.Text += $" | {lineNum}\n";
+                lastLineCount++;
+            }
+
+            while (lines.Length+1 <= lastLineCount && lastLineCount != 1)
+            {
+                if (lineNum > 1)
+                {
+                    string delLineNum = txtLineNums.Text.Replace($" | {lineNum}\n", "");
+                    txtLineNums.Text = delLineNum;
+                }
+                if (lineNum != 1)
+                {
+                    lineNum--;
+                }
+                if (lastLineCount != 1)
+                {
+                    lastLineCount--;
+                }
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            saveBar.Value = 0;
+
+            printLineNums();
+
+            txtLineNums.SelectionStart = txtLineNums.Text.Length;
+            txtLineNums.ScrollToCaret();
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fontSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedFontName = fontSelect.SelectedItem.ToString();
+
+            switch (selectedFontName)
+            {
+                case "Agency FB":
+                    txtMain.Font = new Font("Agency FB", txtMain.Font.Size);
+                    break;
+                case "Arial":
+                    txtMain.Font = new Font("Arial", txtMain.Font.Size);
+                    break;
+                case "Consolas":
+                    txtMain.Font = new Font("Consolas", txtMain.Font.Size);
+                    break;
+                default:
+                    txtMain.Font = new Font("Consolas", 10);
+                    break;
+            }
+        }
+
+        private void fontSizeSelect_IndexChanged(Object sender, EventArgs e)
+        {
+            string selectedFontSize = fontSizeSelect.SelectedItem.ToString();
+
+            switch (selectedFontSize)
+            {
+                case "2":
+                    txtMain.Font = new Font("txtMain.Font", 2);
+                    txtLineNums.Font = new Font("txtLineNums.Font", 2);
+                    break;
+                case "4":
+                    txtMain.Font = new Font("txtMain.Font", 4);
+                    txtLineNums.Font = new Font("txtLineNums.Font", 4);
+                    break;
+                case "6":
+                    txtMain.Font = new Font("txtMain.Font", 6);
+                    txtLineNums.Font = new Font("txtLineNums.Font", 6);
+                    break;
+                case "8":
+                    txtMain.Font = new Font("txtMain.Font", 8);
+                    txtLineNums.Font = new Font("txtLineNums.Font", 8);
+                    break;
+                case "10":
+                    txtMain.Font = new Font("txtMain.Font", 10);
+                    txtLineNums.Font = new Font("txtLineNums.Font", 10);
+                    break;
+                case "12":
+                    txtMain.Font = new Font("txtMain.Font", 12);
+                    txtLineNums.Font = new Font("txtLineNums.Font", 12);
+                    break;
+                case "14":
+                    txtMain.Font = new Font("txtMain.Font", 14);
+                    txtLineNums.Font = new Font("txtLineNums.Font", 14);
+                    break;
+                case "16":
+                    txtMain.Font = new Font("txtMain.Font", 16);
+                    txtLineNums.Font = new Font("txtLineNums.Font", 16);
+                    break;
+                case "18":
+                    txtMain.Font = new Font("txtMain.Font", 18);
+                    txtLineNums.Font = new Font("txtLineNums.Font", 18);
+                    break;
+                case "20":
+                    txtMain.Font = new Font("txtMain.Font", 20);
+                    txtLineNums.Font = new Font("txtLineNums.Font", 20);
+                    break;
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(openFileDialog1.FileName))
+            {
+                saveFileDialog1.FileName = openFileDialog1.FileName;
+            }
+            else
+            {
+                saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog1.FilterIndex = 2;
+                saveFileDialog1.RestoreDirectory = true;
+
+                if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+            }
+
+            try
+            {
+                using (System.IO.StreamWriter sw = new System.IO.StreamWriter(saveFileDialog1.FileName))
+                {
+                    sw.Write(txtMain.Text);
+                }
+
+                saveBar.Value = 100;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName))
+                    {
+                        string fileContents = sr.ReadToEnd();
+                        txtMain.Text = fileContents;
+                    }
+
+                    printLineNums();
+                    saveBar.Value = 100;
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error opening file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
